21c115d49bde37b78c391ce86d8c5dc10a69e750
stenyukovs <s.stenyukov@mixvel.com>
2025-01-06 20:02:43 +0300

    fix: minor

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,13 +21,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class VigenereCipheringMachine {
   constructor(isDirect = true) {
-    this.isDirect = isDirect;  // Set direction (true for direct, false for reverse)
+    this.isDirect = isDirect;
   }
 
   encrypt(message, key) {
-    if (!message || !key) {
+    if (!message || !key)
       throw new Error('Invalid input!');
-    }
 
     message = message.toUpperCase();
     key = key.toUpperCase();
@@ -37,22 +36,18 @@ class VigenereCipheringMachine {
 
     for (let i = 0; i < message.length; i++) {
       let char = message[i];
-
-      // Only encrypt alphabetic characters
       if (/[A-Z]/.test(char)) {
-        let shift = key[keyIndex % key.length].charCodeAt(0) - 65;  // Calculate shift based on key
+        let shift = key[keyIndex % key.length].charCodeAt(0) - 65;
         let encryptedChar = String.fromCharCode(((char.charCodeAt(0) - 65 + shift) % 26) + 65);
         result.push(encryptedChar);
 
-        keyIndex++;  // Move to next key character
-      } else {
-        result.push(char);  // Non-alphabetic characters remain the same
-      }
+        keyIndex++;
+      } else
+        result.push(char);
     }
 
     let encryptedMessage = result.join('');
 
-    // If reverse mode, reverse the final result
     return this.isDirect ? encryptedMessage : encryptedMessage.split('').reverse().join('');
   }
 
