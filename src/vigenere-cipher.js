4aa983b754c2cbcb4cc4cae86d308c2857000367
stenyukovs <s.stenyukov@mixvel.com>
2025-01-06 19:54:48 +0300

    feat: 2 done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;  // Set direction (true for direct, false for reverse)
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Invalid input!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = [];
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i];
+
+      // Only encrypt alphabetic characters
+      if (/[A-Z]/.test(char)) {
+        let shift = key[keyIndex % key.length].charCodeAt(0) - 65;  // Calculate shift based on key
+        let encryptedChar = String.fromCharCode(((char.charCodeAt(0) - 65 + shift) % 26) + 65);
+        result.push(encryptedChar);
+
+        keyIndex++;  // Move to next key character
+      } else {
+        result.push(char);  // Non-alphabetic characters remain the same
+      }
+    }
+
+    let encryptedMessage = result.join('');
+
+    // If reverse mode, reverse the final result
+    return this.isDirect ? encryptedMessage : encryptedMessage.split('').reverse().join('');
   }
-}
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Invalid input!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = [];
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i];
+
+      // Only decrypt alphabetic characters
+      if (/[A-Z]/.test(char)) {
+        let shift = key[keyIndex % key.length].charCodeAt(0) - 65;  // Calculate shift based on key
+        let decryptedChar = String.fromCharCode(((char.charCodeAt(0) - 65 - shift + 26) % 26) + 65);
+        result.push(decryptedChar);
+
+        keyIndex++;  // Move to next key character
+      } else {
+        result.push(char);  // Non-alphabetic characters remain the same
+      }
+    }
+
+    let decryptedMessage = result.join('');
+
+    // If reverse mode, reverse the final result
+    return this.isDirect ? decryptedMessage : decryptedMessage.split('').reverse().join('');
+  }}
 
 module.exports = {
   VigenereCipheringMachine
